aeaa59ecf2e0dc80cc4850cd68bab41595c2cf02
Colin O'Brien <colin@graplsecurity.com>
2022-10-20 19:46:28 -0700

    Clean up warnings

--- autobox-effect-parser/src/ast.rs
+++ autobox-effect-parser/src/ast.rs
@@ -1,42 +1,36 @@
 #![allow(unused_imports)]
 
-use nom::{
-    IResult,
-    error::ParseError,
-    sequence::separated_pair,
-};
 use nom::branch::alt;
-use nom::bytes::complete::{tag, take, take_till, take_till1, take_until, take_until1, take_while, take_while1};
+use nom::bytes::complete::{
+    tag, take, take_till, take_till1, take_until, take_until1, take_while, take_while1,
+};
 use nom::character::complete::{alpha1, alphanumeric1, multispace0};
 use nom::combinator::{map_res, not, opt, recognize};
 use nom::multi::{many0, many0_count, separated_list0};
 use nom::sequence::{delimited, pair, preceded};
+use nom::{error::ParseError, sequence::separated_pair, IResult};
 
 pub fn identifier(input: &str) -> IResult<&str, &str> {
-    recognize(
-        pair(
-            alt((alpha1, tag("_"))),
-            many0_count(alt((alphanumeric1, tag("_")))),
-        )
-    )(input)
+    recognize(pair(
+        alt((alpha1, tag("_"))),
+        many0_count(alt((alphanumeric1, tag("_")))),
+    ))(input)
 }
 
 // Remove whitespace from the beginning and end of a string
-fn ws<'a, F: 'a, O, E: ParseError<&'a str>>(inner: F) -> impl FnMut(&'a str) -> IResult<&'a str, O, E>
-    where
-        F: Fn(&'a str) -> IResult<&'a str, O, E>,
+fn ws<'a, F: 'a, O, E: ParseError<&'a str>>(
+    inner: F,
+) -> impl FnMut(&'a str) -> IResult<&'a str, O, E>
+where
+    F: Fn(&'a str) -> IResult<&'a str, O, E>,
 {
-    delimited(
-        multispace0,
-        inner,
-        multispace0,
-    )
+    delimited(multispace0, inner, multispace0)
 }
 
 #[derive(Debug, Clone)]
 pub struct Arg<'a> {
-    arg_name: &'a str,
-    arg_binding: &'a str,
+    pub arg_name: &'a str,
+    pub arg_binding: &'a str,
 }
 
 impl<'a> Arg<'a> {
@@ -44,35 +38,36 @@ impl<'a> Arg<'a> {
         let (input, arg_name) = ws(identifier)(input)?;
         let (input, arg_binding) = preceded(ws(tag("as")), identifier)(input)?;
 
-        Ok((input, Self { arg_name, arg_binding }))
+        Ok((
+            input,
+            Self {
+                arg_name,
+                arg_binding,
+            },
+        ))
     }
 }
 
-
 #[derive(Clone, Debug, Default)]
 pub struct Args<'a> {
-    args: Vec<Arg<'a>>,
+    pub args: Vec<Arg<'a>>,
 }
 
 impl<'a> Args<'a> {
     fn parse(input: &'a str) -> IResult<&str, Self> {
         let (input, args) = delimited(
             ws(tag("(")),
-            separated_list0(
-                ws(tag(",")),
-                Arg::parse,
-            ),
+            separated_list0(ws(tag(",")), Arg::parse),
             ws(tag(")")),
         )(input)?;
         Ok((input, Self { args }))
     }
 }
 
-
 #[derive(Debug, Clone)]
 pub struct Add<'a> {
-    lhs: Expr<'a>,
-    rhs: Expr<'a>,
+    pub lhs: Expr<'a>,
+    pub rhs: Expr<'a>,
 }
 
 impl<'a> Add<'a> {
@@ -91,23 +86,15 @@ impl<'a> Add<'a> {
 
 #[derive(Debug, Clone)]
 pub struct LitStr<'a> {
-    value: &'a str,
+    pub value: &'a str,
 }
 
 impl<'a> LitStr<'a> {
     fn parse(input: &'a str) -> IResult<&str, Self> {
         println!("litstr input = {:?}", input);
         let (input, value) = alt((
-            delimited(
-                tag("'"),
-                take_till1(|c| c == '\''),
-                tag("'"),
-            ),
-            delimited(
-                tag("\""),
-                take_till1(|c| c == '"'),
-                tag("\""),
-            )
+            delimited(tag("'"), take_till1(|c| c == '\''), tag("'")),
+            delimited(tag("\""), take_till1(|c| c == '"'), tag("\"")),
         ))(input)?;
         println!("litstr value = {:?}", value);
         Ok((input, Self { value }))
@@ -116,7 +103,7 @@ impl<'a> LitStr<'a> {
 
 #[derive(Debug, Clone)]
 pub struct Var<'a> {
-    name: &'a str,
+    pub name: &'a str,
 }
 
 impl<'a> Var<'a> {
@@ -136,21 +123,24 @@ pub enum Expr<'a> {
 }
 
 impl<'a> Expr<'a> {
-    fn unwrap_lit_str(&self) -> &LitStr<'a> {
+    #[track_caller]
+    pub fn unwrap_lit_str(&self) -> &LitStr<'a> {
         match self {
             Expr::LitStr(lit_str) => lit_str,
             _ => panic!("Expected LitStr"),
         }
     }
 
-    fn unwrap_var(&self) -> &Var {
+    #[track_caller]
+    pub fn unwrap_var(&self) -> &Var {
         match self {
             Expr::Var(var) => var,
             _ => panic!("Expected Var"),
         }
     }
 
-    fn unwrap_add(&self) -> &Add<'a> {
+    #[track_caller]
+    pub fn unwrap_add(&self) -> &Add<'a> {
         match self {
             Expr::Add(add) => add,
             _ => panic!("Expected Add"),
@@ -159,10 +149,12 @@ impl<'a> Expr<'a> {
 }
 
 impl<'a> Expr<'a> {
-    fn parse(input: &'a str) -> IResult<&str, Self> {
+    pub fn parse(input: &'a str) -> IResult<&str, Self> {
         println!("Parsing expr: {}", input);
         let (input, expr) = alt((
-            map_res(ws(Add::parse), |add| Ok::<Expr<'_>, &str>(Expr::Add(Box::new(add)))),
+            map_res(ws(Add::parse), |add| {
+                Ok::<Expr<'_>, &str>(Expr::Add(Box::new(add)))
+            }),
             map_res(ws(LitStr::parse), |s| Ok::<Expr<'_>, &str>(Expr::LitStr(s))),
             map_res(ws(Var::parse), |var| Ok::<Expr<'_>, &str>(Expr::Var(var))),
         ))(input)?;
@@ -175,77 +167,76 @@ impl<'a> Expr<'a> {
 
 #[derive(Debug, Clone)]
 pub struct SideEffectStmt<'a> {
-    side_effect_name: &'a str,
-    side_effect_arguments: Vec<Expr<'a>>,
-    binding: Option<&'a str>,
+    pub side_effect_name: &'a str,
+    pub side_effect_arguments: Vec<Expr<'a>>,
+    pub binding: Option<&'a str>,
 }
 
 impl<'a> SideEffectStmt<'a> {
-    fn parse(input: &'a str) -> IResult<&str, Self> {
+    pub fn parse(input: &'a str) -> IResult<&str, Self> {
         let (input, side_effect_name) = ws(identifier)(input)?;
-        let (input, args) = delimited(
-            ws(tag("(")),
-            take_till1(|c| c == ')'),
-            ws(tag(")")),
-        )(input)?;
+        let (input, args) = delimited(ws(tag("(")), take_till1(|c| c == ')'), ws(tag(")")))(input)?;
         println!("side effect args = {:?}", args);
         println!("side effect input = {:?}", input);
-        let (_input, side_effect_arguments) = separated_list0(
-            ws(tag(",")),
-            ws(Expr::parse)
-        )(args)?;
+        let (_input, side_effect_arguments) = separated_list0(ws(tag(",")), ws(Expr::parse))(args)?;
 
-        println!("side effect side_effect_arguments = {:?}", side_effect_arguments);
+        println!(
+            "side effect side_effect_arguments = {:?}",
+            side_effect_arguments
+        );
         println!("side effect input = {:?}", input);
         let (input, binding) = opt(preceded(ws(tag("as")), identifier))(input)?;
-        Ok((input, Self { side_effect_name, side_effect_arguments, binding }))
+        Ok((
+            input,
+            Self {
+                side_effect_name,
+                side_effect_arguments,
+                binding,
+            },
+        ))
     }
 }
 
 #[derive(Debug, Clone)]
 pub struct SideEffects<'a> {
-    side_effect_stmts: Vec<SideEffectStmt<'a>>,
+    pub side_effect_stmts: Vec<SideEffectStmt<'a>>,
 }
 
 impl<'a> SideEffects<'a> {
-    fn parse(input: &'a str) -> IResult<&str, Self> {
+    pub fn parse(input: &'a str) -> IResult<&str, Self> {
         let (input, _) = ws(tag("("))(input)?;
-        let (input, side_effect_stmts) = separated_list0(
-            ws(tag(",")),
-            SideEffectStmt::parse,
-        )(input)?;
+        let (input, side_effect_stmts) =
+            separated_list0(ws(tag(",")), SideEffectStmt::parse)(input)?;
         let (input, _) = ws(tag(")"))(input)?;
         let (input, _) = opt(ws(tag(",")))(input)?;
-        Ok((input, Self {
-            side_effect_stmts,
-        }))
+        Ok((input, Self { side_effect_stmts }))
     }
 }
 
 #[derive(Debug, Clone)]
 pub struct DeclareMacro<'a> {
-    args: Args<'a>,
-    side_effects: SideEffects<'a>,
-    returns: Option<Expr<'a>>,
+    pub args: Args<'a>,
+    pub side_effects: SideEffects<'a>,
+    pub returns: Option<Expr<'a>>,
 }
 
 impl<'a> DeclareMacro<'a> {
-    fn parse(input: &'a str) -> IResult<&str, Self> {
-        println!("{input}");
+    pub fn parse(input: &'a str) -> IResult<&str, Self> {
         let (input, args) = opt(delimited(ws(tag("args=")), Args::parse, ws(tag(","))))(input)?;
-        println!("args: {:?}", args);
-        println!("input: {:?}", input);
-        let (input, side_effects) = preceded(
-            ws(tag("side_effects=")),
-            SideEffects::parse,
-        )(input)?;
+        let (input, side_effects) = preceded(ws(tag("side_effects=")), SideEffects::parse)(input)?;
 
         let (input, returns) = opt(preceded(ws(tag("returns=")), Expr::parse))(input)?;
 
-        Ok((input, Self { args: args.unwrap_or_default(), side_effects, returns }))
+        Ok((
+            input,
+            Self {
+                args: args.unwrap_or_default(),
+                side_effects,
+                returns,
+            },
+        ))
     }
 }
-
 
 #[cfg(test)]
 mod tests {
@@ -275,41 +266,73 @@ mod tests {
         let (rest, side_effect_stmt) = SideEffectStmt::parse("read_file(bar, baz) as qux").unwrap();
         assert_eq!(side_effect_stmt.side_effect_name, "read_file");
         assert_eq!(side_effect_stmt.side_effect_arguments.len(), 2);
-        assert_eq!(side_effect_stmt.side_effect_arguments[0].unwrap_var().name, "bar");
-        assert_eq!(side_effect_stmt.side_effect_arguments[1].unwrap_var().name, "baz");
+        assert_eq!(
+            side_effect_stmt.side_effect_arguments[0].unwrap_var().name,
+            "bar"
+        );
+        assert_eq!(
+            side_effect_stmt.side_effect_arguments[1].unwrap_var().name,
+            "baz"
+        );
         assert_eq!(side_effect_stmt.binding, Some("qux"));
         assert_eq!(rest, "");
     }
 
     #[test]
     fn test_side_effects_parse() {
-        let (rest, side_effects) = SideEffects::parse("\
+        let (rest, side_effects) = SideEffects::parse(
+            "\
             (\
             eval(T + '/') as U,\
             eval(T),\
             read_file(bar, baz) as qux\
             )\
-        ").unwrap();
+        ",
+        )
+        .unwrap();
         assert_eq!(rest, "");
         assert_eq!(side_effects.side_effect_stmts.len(), 3);
         let side_effect_stmt = &side_effects.side_effect_stmts[0];
         assert_eq!(side_effect_stmt.side_effect_name, "eval");
         assert_eq!(side_effect_stmt.side_effect_arguments.len(), 1);
-        assert_eq!(side_effect_stmt.side_effect_arguments[0].unwrap_add().lhs.unwrap_var().name, "T");
-        assert_eq!(side_effect_stmt.side_effect_arguments[0].unwrap_add().rhs.unwrap_lit_str().value, "/");
+        assert_eq!(
+            side_effect_stmt.side_effect_arguments[0]
+                .unwrap_add()
+                .lhs
+                .unwrap_var()
+                .name,
+            "T"
+        );
+        assert_eq!(
+            side_effect_stmt.side_effect_arguments[0]
+                .unwrap_add()
+                .rhs
+                .unwrap_lit_str()
+                .value,
+            "/"
+        );
         assert_eq!(side_effect_stmt.binding, Some("U"));
 
         let side_effect_stmt = &side_effects.side_effect_stmts[1];
         assert_eq!(side_effect_stmt.side_effect_name, "eval");
         assert_eq!(side_effect_stmt.side_effect_arguments.len(), 1);
-        assert_eq!(side_effect_stmt.side_effect_arguments[0].unwrap_var().name, "T");
+        assert_eq!(
+            side_effect_stmt.side_effect_arguments[0].unwrap_var().name,
+            "T"
+        );
         assert_eq!(side_effect_stmt.binding, None);
 
         let side_effect_stmt = &side_effects.side_effect_stmts[2];
         assert_eq!(side_effect_stmt.side_effect_name, "read_file");
         assert_eq!(side_effect_stmt.side_effect_arguments.len(), 2);
-        assert_eq!(side_effect_stmt.side_effect_arguments[0].unwrap_var().name, "bar");
-        assert_eq!(side_effect_stmt.side_effect_arguments[1].unwrap_var().name, "baz");
+        assert_eq!(
+            side_effect_stmt.side_effect_arguments[0].unwrap_var().name,
+            "bar"
+        );
+        assert_eq!(
+            side_effect_stmt.side_effect_arguments[1].unwrap_var().name,
+            "baz"
+        );
         assert_eq!(side_effect_stmt.binding, Some("qux"));
     }
 
@@ -318,8 +341,19 @@ mod tests {
     fn test_expr_nested_parens() {
         let (rest, expr) = Expr::parse("((T + '/') + U)").unwrap();
         assert_eq!(rest, "");
-        assert_eq!(expr.unwrap_add().lhs.unwrap_add().lhs.unwrap_var().name, "T");
-        assert_eq!(expr.unwrap_add().lhs.unwrap_add().rhs.unwrap_lit_str().value, "/");
+        assert_eq!(
+            expr.unwrap_add().lhs.unwrap_add().lhs.unwrap_var().name,
+            "T"
+        );
+        assert_eq!(
+            expr.unwrap_add()
+                .lhs
+                .unwrap_add()
+                .rhs
+                .unwrap_lit_str()
+                .value,
+            "/"
+        );
         assert_eq!(expr.unwrap_add().rhs.unwrap_lit_str().value, "U");
     }
 
@@ -328,8 +362,19 @@ mod tests {
     fn test_expr_chain() {
         let (rest, expr) = Expr::parse("(T + '/' + U").unwrap();
         assert_eq!(rest, "");
-        assert_eq!(expr.unwrap_add().lhs.unwrap_add().lhs.unwrap_var().name, "T");
-        assert_eq!(expr.unwrap_add().lhs.unwrap_add().rhs.unwrap_lit_str().value, "/");
+        assert_eq!(
+            expr.unwrap_add().lhs.unwrap_add().lhs.unwrap_var().name,
+            "T"
+        );
+        assert_eq!(
+            expr.unwrap_add()
+                .lhs
+                .unwrap_add()
+                .rhs
+                .unwrap_lit_str()
+                .value,
+            "/"
+        );
         assert_eq!(expr.unwrap_add().rhs.unwrap_lit_str().value, "U");
     }
 
@@ -351,20 +396,86 @@ mod tests {
         assert_eq!(declare_macro.args.args[1].arg_name, "baz");
         assert_eq!(declare_macro.args.args[1].arg_binding, "F");
         assert_eq!(declare_macro.side_effects.side_effect_stmts.len(), 3);
-        assert_eq!(declare_macro.side_effects.side_effect_stmts[0].side_effect_name, "eval");
-        assert_eq!(declare_macro.side_effects.side_effect_stmts[0].side_effect_arguments.len(), 1);
-        assert_eq!(declare_macro.side_effects.side_effect_stmts[0].side_effect_arguments[0].unwrap_add().lhs.unwrap_var().name, "F");
-        assert_eq!(declare_macro.side_effects.side_effect_stmts[0].side_effect_arguments[0].unwrap_add().rhs.unwrap_lit_str().value, "/");
-        assert_eq!(declare_macro.side_effects.side_effect_stmts[0].binding, Some("FS"));
-        assert_eq!(declare_macro.side_effects.side_effect_stmts[1].side_effect_name, "eval");
-        assert_eq!(declare_macro.side_effects.side_effect_stmts[1].side_effect_arguments.len(), 1);
-        assert_eq!(declare_macro.side_effects.side_effect_stmts[1].side_effect_arguments[0].unwrap_add().lhs.unwrap_var().name, "FS");
-        assert_eq!(declare_macro.side_effects.side_effect_stmts[1].side_effect_arguments[0].unwrap_add().rhs.unwrap_var().name, "B");
-        assert_eq!(declare_macro.side_effects.side_effect_stmts[1].binding, Some("result"));
-        assert_eq!(declare_macro.side_effects.side_effect_stmts[2].side_effect_name, "read_file");
-        assert_eq!(declare_macro.side_effects.side_effect_stmts[2].side_effect_arguments.len(), 1);
-        assert_eq!(declare_macro.side_effects.side_effect_stmts[2].side_effect_arguments[0].unwrap_var().name, "result");
-        assert_eq!(declare_macro.side_effects.side_effect_stmts[2].binding, None);
+        assert_eq!(
+            declare_macro.side_effects.side_effect_stmts[0].side_effect_name,
+            "eval"
+        );
+        assert_eq!(
+            declare_macro.side_effects.side_effect_stmts[0]
+                .side_effect_arguments
+                .len(),
+            1
+        );
+        assert_eq!(
+            declare_macro.side_effects.side_effect_stmts[0].side_effect_arguments[0]
+                .unwrap_add()
+                .lhs
+                .unwrap_var()
+                .name,
+            "F"
+        );
+        assert_eq!(
+            declare_macro.side_effects.side_effect_stmts[0].side_effect_arguments[0]
+                .unwrap_add()
+                .rhs
+                .unwrap_lit_str()
+                .value,
+            "/"
+        );
+        assert_eq!(
+            declare_macro.side_effects.side_effect_stmts[0].binding,
+            Some("FS")
+        );
+        assert_eq!(
+            declare_macro.side_effects.side_effect_stmts[1].side_effect_name,
+            "eval"
+        );
+        assert_eq!(
+            declare_macro.side_effects.side_effect_stmts[1]
+                .side_effect_arguments
+                .len(),
+            1
+        );
+        assert_eq!(
+            declare_macro.side_effects.side_effect_stmts[1].side_effect_arguments[0]
+                .unwrap_add()
+                .lhs
+                .unwrap_var()
+                .name,
+            "FS"
+        );
+        assert_eq!(
+            declare_macro.side_effects.side_effect_stmts[1].side_effect_arguments[0]
+                .unwrap_add()
+                .rhs
+                .unwrap_var()
+                .name,
+            "B"
+        );
+        assert_eq!(
+            declare_macro.side_effects.side_effect_stmts[1].binding,
+            Some("result")
+        );
+        assert_eq!(
+            declare_macro.side_effects.side_effect_stmts[2].side_effect_name,
+            "read_file"
+        );
+        assert_eq!(
+            declare_macro.side_effects.side_effect_stmts[2]
+                .side_effect_arguments
+                .len(),
+            1
+        );
+        assert_eq!(
+            declare_macro.side_effects.side_effect_stmts[2].side_effect_arguments[0]
+                .unwrap_var()
+                .name,
+            "result"
+        );
+        assert_eq!(
+            declare_macro.side_effects.side_effect_stmts[2].binding,
+            None
+        );
     }
 
     #[test]
@@ -397,7 +508,6 @@ mod tests {
         assert_eq!(add_op.lhs.unwrap_lit_str().value, "foo");
         assert_eq!(add_op.rhs.unwrap_var().name, "bar");
     }
-
 
     #[test]
     fn test_expr_add_nested_parse() {
--- autobox-effect/src/lib.rs
+++ autobox-effect/src/lib.rs
@@ -2,16 +2,16 @@ extern crate proc_macro;
 use proc_macro::TokenStream;
 
 #[proc_macro_attribute]
-pub fn declare(attr: TokenStream, item: TokenStream) -> TokenStream {
+pub fn declare(_attr: TokenStream, item: TokenStream) -> TokenStream {
     item
 }
 
 #[proc_macro_attribute]
-pub fn entrypoint(attr: TokenStream, item: TokenStream) -> TokenStream {
+pub fn entrypoint(_attr: TokenStream, item: TokenStream) -> TokenStream {
     item
 }
 
 #[proc_macro_attribute]
-pub fn infer(attr: TokenStream, item: TokenStream) -> TokenStream {
+pub fn infer(_attr: TokenStream, item: TokenStream) -> TokenStream {
     item
 }
